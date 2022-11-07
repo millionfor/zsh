328093f875edcb7af43df6721973b8bc2bb0d3c5
QuanQuan <millionfor@apache.org>
2022-11-07 11:17:35 +0800

    fix: up

--- config/zsh.zsh
+++ config/zsh.zsh
@@ -1,7 +1,1 @@
-source $ZSH/omz.sh
 source $ZSH/themes/simple.zsh-theme
-source $ZSH/plugins/z/z.plugin.zsh
-source $ZSH/plugins/extract/extract.plugin.zsh
-source $ZSH/plugins/fzf-tab/fzf-tab.zsh
-source $ZSH/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-source $ZSH/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
--- init.zsh
+++ init.zsh
@@ -1,9 +1,5 @@
 export ZSH=~/.config/zsh
-source $ZSH/config/exports.zsh
-source $ZSH/config/git.zsh
 source $ZSH/config/zsh.zsh
-source $ZSH/config/fzf.zsh
-# source $ZSH/config/hook.zsh
 source $ZSH/config/shortcut.zsh
 
 [ -f $ZSH/config/private.zsh ] && source $ZSH/config/private.zsh
