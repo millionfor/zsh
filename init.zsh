f20fda92a577176e97af31342ed1cca57058be84
QuanQuan <millionfor@apache.org>
2023-03-08 13:52:03 +0800

    fix: up

--- init.zsh
+++ init.zsh
@@ -5,10 +5,10 @@ export OMZ=$(cd $(dirname $0);pwd)
 source $OMZ/config/git.zsh
 source $OMZ/config/omz.zsh
 source $OMZ/config/fzf.zsh
-source $OMZ/config/hook.zsh
+# source $OMZ/config/hook.zsh
 source $OMZ/themes/simple.zsh-theme
 
-export _OMZ_APPLY_CHPWD_HOOK=false
+# export _OMZ_APPLY_CHPWD_HOOK=true
 
 source $ZSH/config/simple.zsh-theme
 
