eee17db911f2d15c0b15c6024ace95a9a57a6777
QuanQuan <millionfor@apache.org>
2023-03-22 09:31:29 +0800

    fix: up

--- init.zsh
+++ init.zsh
@@ -1,7 +1,6 @@
 export ZSH=~/.config/zsh
 export OMZ=~/.config/zsh
 source $ZSH/config/shortcut.zsh
-source $ZSH/config/exports.zsh
 source $ZSH/config/omz.zsh
 source $ZSH/config/fzf.zsh
 source $ZSH/themes/*.zsh-theme
